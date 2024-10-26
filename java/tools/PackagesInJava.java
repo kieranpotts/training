07307940c3c85b6fde486bfe1e5c3a3ae9e4b355
Kieran Potts <hello@kieranpotts.com>
2024-10-26 12:11:36 +0100

    feature: java - packages

---
+++ java/tools/PackagesInJava.java
@@ -0,0 +1,23 @@
+/*
+Almost all classes (and other constructs) within an application or library
+SHOULD be part of package.
+*/
+package tools;
+
+/*
+When creating libraries for consumption in other projects, you need to
+create a unique "namespace" for all your classes, so they are unique not
+only in the current project but in *all* Java projects the world over.
+
+A convention is to flip your domain name, so "google.com" becomes "com.google".
+
+----
+package com.google.java;
+----
+*/
+
+public class PackagesInJava {
+  public static void main(String[] args) {
+    // …
+  }
+}
