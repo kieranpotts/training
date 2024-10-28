d9d442735223e5128ed5b86c1078a29e8d4cf840
Kieran Potts <hello@kieranpotts.com>
2024-10-28 09:51:22 +0000

    feature: java - relational operators

---
+++ java/RelationalOperatorsInJava.java
@@ -0,0 +1,27 @@
+public class RelationalOperatorsInJava {
+  public static void main(String[] args) {
+
+    /*
+    Relational operators are used to compare two values and return a
+    boolean value (true or false).
+    */
+
+    int x = 6;
+    int y = 5;
+
+    /* < Less than */
+    boolean result1 = x < y;
+    System.out.println(result1); // false
+
+    /* > Greater than */
+    boolean result2 = x > y;
+    System.out.println(result2); // true
+
+    /*
+    <= Less than or equal to
+    >= Greater than or equal to
+    == Equal to
+    != Not equal to
+    */
+  }
+}
